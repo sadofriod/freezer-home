c6f643d30d8fbd6d1afd829ad65cdc31f3d1b7b1
ashesborn <792634614@qq.com>
2024-12-18 23:45:07 +0800

    add google analytic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,12 @@ import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
 import { nextI18nConf } from "../../next-i18n-conf";
 import { AppCacheProvider } from '@mui/material-nextjs/v15-pagesRouter'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <AppCacheProvider>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </AppCacheProvider>
   )
